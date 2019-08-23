74f712d2c23ffcb5863faa27f74320ca5aeb3ad2
Austin Schrader <Austin.Schrader@FlandersCapital.com>
2019-08-22 17:20:03 -0700

    Added notes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,15 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("10 plus 20 is " + 10 + 20);
+
+
+
+
+
+		/*
+		Java is strictly typed
+
+
+		*/
     }
 }
